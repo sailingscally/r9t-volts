04b1133f99e2fac14283a53925ffa93c899e6469
Luis Martins <luis.martins@gmail.com>
2022-11-05 02:52:05 +0000

    Refactoring 'app.js'.

--- app.js
+++ app.js
@@ -25,57 +25,53 @@ const { Channel } = require('ads1015');
 const ads1015 = require('ads1015');
 const mqtt = require('mqtt');
 
-const run = async () => {
-  console.log('Starting R nineT voltage monitor...')
-  await ads1015.init();
+console.log('Starting R nineT voltage monitor...')
+ads1015.init();
 
-  const client = mqtt.connect('mqtt://localhost:1883', {
-    clientId: 'r9t-volts'
-  });
+const client = mqtt.connect('mqtt://localhost:1883', {
+  clientId: 'r9t-volts'
+});
 
-  let connected = false;
+let connected = false;
 
-  client.on('connect', () => {
-    console.log('Connected to MQTT broker.');
+client.on('connect', () => {
+  console.log('Connected to MQTT broker.');
 
-    client.options.reconnectPeriod = 1000;
-    connected = true;
-  });
+  client.options.reconnectPeriod = 1000;
+  connected = true;
+});
 
-  client.on('close', () => {
-    if(connected) {
-      console.log('Connection to MQTT broker lost.');
-      connected = false;
-    }
-  });
+client.on('close', () => {
+  if(connected) {
+    console.log('Connection to MQTT broker lost.');
+    connected = false;
+  }
+});
 
-  client.on('error', (error) => {
-    switch(error.code) {
-      case 'ECONNREFUSED':
-        console.log(`Unable to connect to MQTT broker on ${error.address}:${error.port}.`);
-        break;
+client.on('error', (error) => {
+  switch(error.code) {
+    case 'ECONNREFUSED':
+      console.log(`Unable to connect to MQTT broker on ${error.address}:${error.port}.`);
+      break;
 
-      default:
-        console.log(error);
-        break;
-    }
-  });
+    default:
+      console.log(error);
+      break;
+  }
+});
 
-  /*
-   * Increase the time between connection attempts
-   */
-  client.on('reconnect', () => {
-    client.options.reconnectPeriod *= 2;
-  });
+/*
+ * Increase the time between connection attempts
+ */
+client.on('reconnect', () => {
+  client.options.reconnectPeriod *= 2;
+});
 
-  const job = setInterval(async () => {
-    const value = await ads1015.measure(Channel.ZERO, Gain.ONE);
-    console.log('Voltage [0-main]: ' + value.toFixed(2));
+setInterval(async () => {
+  const value = await ads1015.measure(Channel.ZERO, Gain.ONE);
+  console.log('Voltage [0-main]: ' + value.toFixed(2));
 
-    if(connected) {
-      client.publish('voltage/main', value.toString());
-    }
-  }, 10000);
-}
-
-run();
+  if(connected) {
+    client.publish('voltage/main', value.toString());
+  }
+}, 10000);
